8e564041865f9405bd7dda250f3d23fb09000118
Chris Filipowski <kfilipowski+git@gmail.com>
2019-02-03 14:06:14 +0800

    add angular img comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularImgComponent } from './angular-img/angular-img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularImgComponent
   ],
   imports: [
     BrowserModule,
